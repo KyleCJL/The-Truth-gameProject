2f8524dd3fca1eafa0ec755025d540616aadc903
Kyle Chong Jia Le <kylechongjiale@gmail.com>
2024-08-07 00:08:48 +0800

    added dialogue system

---
+++ The Truth/Assets/Testing/Scripts/Testing_Architect.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TESTING
+{
+    public class Testing_Architect : MonoBehaviour
+    {
+        DialogueSystem ds;
+        TextArchitect architect;
+
+        string[] lines = new string[5]
+        {
+            "你就是教授派下来的助手？",
+            "那你去帮我泡一杯咖啡吧。",
+            "“好吧，让我看有什么简单的案子吧。",
+            "嗯？那我看看。",
+            "行吧，我们出发吧"
+        };
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            ds = DialogueSystem.instance;
+            architect = new TextArchitect(ds.dialogueContainer.dialogueText);
+            architect.buildMethod = TextArchitect.BuildMethod.typewriter;
+            ///architect.speed = 0.5f;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (architect.isBuilding)
+                {
+                    if (!architect.hurryUp)
+                        architect.hurryUp = true;
+                    else
+                        architect.ForceComplete();
+                }
+                else
+                    architect.Build(lines[Random.Range(0,lines.Length)]);
+            }
+            else if (Input.GetKeyDown(KeyCode.A))
+            {
+                architect.Append(lines[Random.Range(0,lines.Length)]);
+            }
+        }
+    }
+}
